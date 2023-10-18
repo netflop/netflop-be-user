85c3c2516233443084edf80766eb4f4c17c139ee
Yora <yoralong@gmail.com>
2023-10-18 15:34:23 +0700

    Revert "Initial project"
    This reverts commit 1d9d654ed2c6621804cf7b08f00efa590431722c.

--- src/main/java/netflop/be/user/controller/CognitoController.java
+++
@@ -1,7 +0,0 @@
-package netflop.be.user.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-@RequestMapping("/auth")
-public class CognitoController {
-}
